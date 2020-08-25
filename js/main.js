cf04730ba3972fe6a3daacc31f85f313bd5bdaae
garmanika <aleks.orlov97@gmail.com>
2020-08-25 19:54:12 +0300

    JetRo

---
+++ js/main.js
@@ -0,0 +1,89 @@
+$(function(){
+
+  $('.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="image/prev.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="image/next.svg" alt=""></button>',
+    asNavFor: '.slider__min',
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+       arrows:false
+        }
+      },
+          ]
+  });
+  $('.slider__min').slick({
+    arrows: false,
+    asNavFor: '.slider__inner',
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    centerMode: false,
+    focusOnSelect: true,
+    centerPadding: '50px',
+    variableWidth: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 5
+         
+        }
+      }
+          ]
+  });
+  $('.header__menu-btn').on('click', function(){
+    $('.header__navigation > ul').slideToggle();
+  });
+
+  $('.works__inner').slick({
+    slidesToShow: 4,
+    infinite: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+          ]
+  });
+    
+  });
+  
+
+    
+    
